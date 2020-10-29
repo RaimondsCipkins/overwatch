151ebbd57f3d8d1994899cdb54bf5fd39b8a47d9
Raimonds Cipkins <rcipkins@gmail.com>
2020-10-29 16:40:37 +0200

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-10-27 21:37:41 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-10-27T21:37:41+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-10-27T22:08:38+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-10-27T22:08:46+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-10-27T22:12:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
